be0185c7a2beb5f0f458d2c146f8446e63ca9fd3
LuckyBros205 <luckybros.205@gmail.com>
2023-11-15 11:50:09 -0500

    obtener usuario login y sesión

--- src/main/java/com/curso/ecommerce/service/IUsuarioService.java
+++ src/main/java/com/curso/ecommerce/service/IUsuarioService.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface IUsuarioService {
     Optional<Usuario> findById(Integer id);
     Usuario save(Usuario usuario);
+    Optional<Usuario> findByEmail(String email);
 }
