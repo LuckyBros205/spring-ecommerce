d6bd6e1d77638caadc336751a97c4adaae1858e4
LuckyBros205 <luckybros.205@gmail.com>
2023-11-17 00:50:23 -0500

    ver detalle de compra al user

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -8,9 +8,11 @@ import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -76,5 +78,18 @@ public class UsuarioController {
         model.addAttribute("ordenes", ordenes);
 
         return "usuario/compras";
+    }
+
+    @GetMapping("/detalle/{id}")
+    public String detalleCompra(@PathVariable Integer id, HttpSession session, Model model){
+        logger.info("Id de la orden: {}", id);
+        Optional<Orden> orden = ordenService.findById(id);
+
+        model.addAttribute("detalles", orden.get().getDetalle());
+
+        //sesion
+        model.addAttribute("sesion", session.getAttribute("idusuario"));
+
+        return "usuario/detallecompra";
     }
 }
--- src/main/java/com/curso/ecommerce/service/IOrdenService.java
+++ src/main/java/com/curso/ecommerce/service/IOrdenService.java
@@ -5,9 +5,11 @@ import com.curso.ecommerce.model.Usuario;
 import org.aspectj.weaver.ast.Or;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IOrdenService {
     List<Orden> findAll();
+    Optional<Orden> findById(Integer id);
     Orden save(Orden orden);
     String generarNumeroOrden();
     List<Orden> findByUsuario(Usuario usuario);
